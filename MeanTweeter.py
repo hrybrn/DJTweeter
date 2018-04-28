fcee542dd806d68e7d01f338a4b875a6039cef96
Unknown <harryjgbrown@gmail.com>
2018-04-28 23:11:11 +0100

    bot is golden

---
+++ MeanTweeter.py
@@ -0,0 +1,51 @@
+import json
+import random
+import twitter
+import time
+
+class MeanTweeter:
+    def __init__(self, peopleLoc, phrasesLoc, secretsLoc):
+        self.peopleLoc = peopleLoc
+        self.phrasesLoc = phrasesLoc
+
+        self.secrets = json.load(open('secrets.json'))
+        
+        self.replied = list()
+
+    def getApi(self):
+        return twitter.Api(consumer_key=self.secrets.consumer_key,
+                  consumer_secret=self.secrets.consumer_secret,
+                  access_token_key=self.secrets.access_token_key,
+                  access_token_secret=self.secrets.access_token_secret)
+
+    def reply(self, statusID):
+        phrasefile = json.load(open(self.phrasesLoc))
+        phrases = phrasefile.phrases
+
+        phraseid = random.randint(0,len(phrases) - 1)
+        phrase = phrases[phraseid]
+
+        self.getApi().PostUpdate(status=phrase,
+            in_reply_to_status_id=statusID,
+            auto_populate_reply_metadata=True)
+
+    def go(self):
+        peoplefile = json.load(open(self.peopleLoc))
+        people = peoplefile.people
+
+        for person in people:
+            query = "q=from%3A" + person
+            results = self.getApi().GetSearch(
+                raw_query=query)
+
+            if len(results) > 0:
+                latest = results[0]
+
+                if latest not in self.replied:
+                    self.reply(latest.id)
+                    self.replied.append(latest.id)
+
+    def run(self):
+        while True:
+            time.sleep(300)
+            self.go()
