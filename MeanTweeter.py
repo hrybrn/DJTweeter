b0cb3d1e939e07b9df7648420a296ee96511f100
Unknown <harryjgbrown@gmail.com>
2018-04-28 23:25:27 +0100

    thought i could be clever but turns out i dont know python that well

--- MeanTweeter.py
+++ MeanTweeter.py
@@ -13,14 +13,14 @@ class MeanTweeter:
         self.replied = list()
 
     def getApi(self):
-        return twitter.Api(consumer_key=self.secrets.consumer_key,
-                  consumer_secret=self.secrets.consumer_secret,
-                  access_token_key=self.secrets.access_token_key,
-                  access_token_secret=self.secrets.access_token_secret)
+        return twitter.Api(consumer_key=self.secrets['consumer_key'],
+                  consumer_secret=self.secrets['consumer_secret'],
+                  access_token_key=self.secrets['access_token_key'],
+                  access_token_secret=self.secrets['access_token_secret'])
 
     def reply(self, statusID):
         phrasefile = json.load(open(self.phrasesLoc))
-        phrases = phrasefile.phrases
+        phrases = phrasefile['phrases']
 
         phraseid = random.randint(0,len(phrases) - 1)
         phrase = phrases[phraseid]
@@ -31,7 +31,7 @@ class MeanTweeter:
 
     def go(self):
         peoplefile = json.load(open(self.peopleLoc))
-        people = peoplefile.people
+        people = peoplefile['people']
 
         for person in people:
             query = "q=from%3A" + person
